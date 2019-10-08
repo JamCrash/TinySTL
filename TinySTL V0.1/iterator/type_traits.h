63d5a50023b6155d84c6bf32110009b4e1b4eb07
han <wangyihanby@163.com>
2019-10-08 21:06:13 +0800

    type_traits

---
+++ TinySTL V0.1/iterator/type_traits.h
@@ -0,0 +1,137 @@
+
+#ifndef TYPE_TRAITS
+#define TYPE_TRAITS
+
+struct __true_type {};
+struct __false_type {};
+
+template<typename _Tp>
+struct __type_traits 
+{
+  // 参考 SGI
+  typedef __true_type this_dummy_must_be_first;
+  /* Do not remove this member. It informs a compiler which
+  automatically specializes __type_traits that this
+  __type_traits template is special. It just makes sure that
+  things work if an implementation is using a template
+  called __type_traits for something unrelated. */
+  
+  typedef __false_type has_trivial_default_constructor;
+  typedef __false_type has_trivial_constructor;
+  typedef __false_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<bool>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<char>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<unsigned char>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<signed char>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<short>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<unsigned short>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<int>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<unsigned int>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<long>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<unsigned long>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<long long>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<unsigned long long>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<float>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<double>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<> struct __type_traits<long double>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+template<typename _Tp>
+struct __type_traits<_Tp*>
+{
+  typedef __true_type has_trivial_default_constructor;
+  typedef __true_type has_trivial_constructor;
+  typedef __true_type has_trivial_destructor;
+};
+
+#endif  /* TYPE_TRAITS */
